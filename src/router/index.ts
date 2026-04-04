90a34a2b32274569f85b9e0b0de52fdcf7370cca
HassanMahmoud-dev <hmido2622@gmail.com>
2026-04-04 20:48:09 +0200

    v1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PortfolioView from '../views/portfolioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'portfolio',
+      component: PortfolioView,
+    },
+  ],
 })
 
 export default router
